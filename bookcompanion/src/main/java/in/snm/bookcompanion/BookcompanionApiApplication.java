23967f56e038996af620e5c7b8da91bf1a8fcb65
Shahid Nazir <shahidnazirmhd@gmail.com>
2024-05-01 22:47:33 +0530

    Spring initialized

---
+++ bookcompanion/src/main/java/in/snm/bookcompanion/BookcompanionApiApplication.java
@@ -0,0 +1,13 @@
+package in.snm.bookcompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcompanionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcompanionApiApplication.class, args);
+	}
+
+}
