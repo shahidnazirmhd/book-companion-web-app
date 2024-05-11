6981bf1568c35b81c4571ec2f8cf8b89bedbd0aa
Shahid Nazir <shahidnazirmhd@gmail.com>
2024-05-11 22:23:42 +0530

    Token interceptor Impl

---
+++ book-companion-ui/src/app/services/interceptor/http-token.interceptor.ts
@@ -0,0 +1,20 @@
+import {HttpHeaders, HttpInterceptorFn} from '@angular/common/http';
+import {inject} from "@angular/core";
+import {TokenService} from "../token/token.service";
+
+export const httpTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const tokenService = inject(TokenService);
+  const token = tokenService.token;
+  if (token) {
+    const authReq = req.clone({
+      // headers: new HttpHeaders({
+      //   Authorization: 'Bearer ' + token
+      // })
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(authReq);
+  }
+  return next(req);
+};
