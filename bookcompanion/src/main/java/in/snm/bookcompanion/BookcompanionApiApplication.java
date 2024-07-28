f1032cfdf00c5b46b89d58eda28218586c5bc26e
Shahid Nazir <shahidnazirmhd@gmail.com>
2024-07-28 13:19:40 +0530

    Backend dockerized

--- bookcompanion/src/main/java/in/snm/bookcompanion/BookcompanionApiApplication.java
+++ bookcompanion/src/main/java/in/snm/bookcompanion/BookcompanionApiApplication.java
@@ -18,13 +18,13 @@ public class BookcompanionApiApplication {
 		SpringApplication.run(BookcompanionApiApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner runner(RoleRepository roleRepository) {
-		return args -> {
-			if (roleRepository.findByName("USER").isEmpty()) {
-				roleRepository.save(Role.builder().name("USER").build());
-			}
-		};
-	}
+//	@Bean
+//	public CommandLineRunner runner(RoleRepository roleRepository) {
+//		return args -> {
+//			if (roleRepository.findByName("USER").isEmpty()) {
+//				roleRepository.save(Role.builder().name("USER").build());
+//			}
+//		};
+//	}
 
 }
